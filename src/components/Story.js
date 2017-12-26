58159c656e7e9909f44ad1aedb89473f9b169e95
Lena Venable <venablena@gmail.com>
2017-12-25 16:53:50 -0800

    start configuring story state

--- src/components/Story.js
+++ src/components/Story.js
@@ -16,28 +16,49 @@ const allEmojis = [
       ]
 
 class Story extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       "user" : {
         emojis: allEmojis,
         points: 0,
       },
       "story": {
-        text: "The mermaid gave the wizard a kiss"
+        text: "The mermaid gave the wizard a kiss",
+        emojis: []
       }
     }
   }
 
+//on click, word is replaced by an emoji, if user has a matching one
   gameplay = (e) => {
-    //removes an empty space at the end of the word
+    //removes the empty space at the end of each word
     const word = e.target.innerHTML.slice(0, -1)
     const match = this.state.user.emojis.find(el => el.name === word)
     if(match) e.target.innerHTML = match.symbol
   }
 
+//configures the gameplay based on the emoji count of the story and the emojis the user already has
+  setTheStage = (story) => {
+    const storyEmojis = this.findEmojis(story)
+    if(storyEmojis.length === 0) console.log("Looks like you don't have any matching emojis. Do you want to try another story or get more emojis?")
+    console.log(storyEmojis);
+    //this.setState({story: {emojis: storyEmojis}})
+  }
+
+//checks which emojis in the story match user emojis
+  findEmojis = (story) => {
+    let result = []
+    for (let i = 0; i < story.length; i++) {
+      const match = this.state.user.emojis.find(el => el.name === story[i])
+      if(match)result.push(match)
+    }
+    return result
+  }
+
   render() {
-    const array = this.state.story.text.split(" ")
+    const storyArray = this.state.story.text.split(" ")
+    this.setTheStage(storyArray)
 
     return (
       <div className="App">
@@ -49,7 +70,7 @@ class Story extends Component {
           {this.state.user.emojis.map(el => <div key={el.id}>{el.symbol}</div> )}
         </div>
         <div className="story">
-          {array.map((el,i) => <span key={i} onClick ={(e) => this.gameplay(e)}>{el + " "}</span>)}
+          {storyArray.map((el,i) => <span key={i} onClick ={(e) => this.gameplay(e)}>{el} </span>)}
         </div>
       </div>
     )
